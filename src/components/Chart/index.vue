cd018be46421d49325664f2596b28eee9fc6446d
Hashim99 <hashimghamdi@outlook.com>
2022-05-30 14:16:00 +0300

    embed iframe test

--- src/components/Chart/index.vue
+++ src/components/Chart/index.vue
@@ -1,183 +1,218 @@
 <template>
   <div class="position-relative h-100 w-100">
-    <canvas
-      ref="chartCanvas"
-      class="p-2 mh-100 w-auto m-auto"
-    />
+   <!--  <canvas ref="chartCanvas" class="p-2 mh-100 w-auto m-auto" /> -->
+
+    <iframe
+    class="mh-150 w-auto ml-5"
+      src="https://www.openstreetmap.org/export/embed.html?bbox=-0.004017949104309083%2C51.47612752641776%2C0.00030577182769775396%2C51.478569861898606&layer=mapnik"
+      title="W3Schools Free Online Web Tutorials"
+    ></iframe>
   </div>
 </template>
 
 <script>
-import { mapGetters } from 'vuex'
-import { chartConstructor } from 'corteza-webapp-compose/src/lib/charts'
-import ChartJS from 'chart.js'
-import Funnel from 'chartjs-plugin-funnel'
-import Gauge from 'chartjs-gauge'
-import csc from 'chartjs-plugin-colorschemes'
-import { compose, NoID } from '@cortezaproject/corteza-js'
+import { mapGetters } from "vuex";
+import { chartConstructor } from "corteza-webapp-compose/src/lib/charts";
+import ChartJS from "chart.js";
+import Funnel from "chartjs-plugin-funnel";
+import Gauge from "chartjs-gauge";
+import csc from "chartjs-plugin-colorschemes";
+import { compose, NoID } from "@cortezaproject/corteza-js";
 
 export default {
   i18nOptions: {
-    namespaces: 'notification',
+    namespaces: "notification"
   },
 
   props: {
     chart: {
       type: Object,
-      default: null,
+      default: null
     },
     reporter: {
       type: Function,
-      default: null,
-    },
+      default: null
+    }
   },
 
-  data () {
+  data() {
     return {
-      renderer: null,
-    }
+      renderer: null
+    };
   },
 
   computed: {
     ...mapGetters({
-      getModuleByID: 'module/getByID',
-      getUserByID: 'user/findByID',
-    }),
+      getModuleByID: "module/getByID",
+      getUserByID: "user/findByID"
+    })
   },
 
   watch: {
-    'chart.chartID': {
+    "chart.chartID": {
       immediate: true,
-      handler (chartID = NoID) {
+      handler(chartID = NoID) {
         if (chartID !== NoID) {
           this.$nextTick(() => {
-            this.updateChart()
-          })
+            this.updateChart();
+          });
         }
-      },
-    },
+      }
+    }
   },
 
-  mounted () {
-    this.$root.$on('chart.update', this.requestChartUpdate)
+  mounted() {
+    this.$root.$on("chart.update", this.requestChartUpdate);
   },
 
-  beforeDestroy () {
+  beforeDestroy() {
     if (this.renderer) {
-      this.renderer.destroy()
+      this.renderer.destroy();
     }
   },
 
   methods: {
-    async updateChart () {
-      const chart = chartConstructor(this.chart)
+    async updateChart() {
+      const chart = chartConstructor(this.chart);
 
       try {
-        chart.isValid()
+        chart.isValid();
 
-        const data = await chart.fetchReports({ reporter: this.reporter })
+        const data = await chart.fetchReports({ reporter: this.reporter });
         if (!!data.labels && Array.isArray(data.labels)) {
           // Get dimension field kind
-          const [report = {}] = this.chart.config.reports
-          const [dimension = {}] = report.dimensions
-          let { field } = dimension
-          const module = this.getModuleByID(report.moduleID)
+          const [report = {}] = this.chart.config.reports;
+          const [dimension = {}] = report.dimensions;
+          let { field } = dimension;
+          const module = this.getModuleByID(report.moduleID);
 
           if (module) {
-            field = [
-              ...module.fields,
-              ...module.systemFields(),
-            ].find(({ name }) => name === field)
+            field = [...module.fields, ...module.systemFields()].find(
+              ({ name }) => name === field
+            );
           }
 
-          if (field && ['User', 'Record'].includes(field.kind)) {
-            if (field.kind === 'User') {
+          if (field && ["User", "Record"].includes(field.kind)) {
+            if (field.kind === "User") {
               // Fetch and map users to labels
-              await this.$store.dispatch('user/fetchUsers', data.labels)
+              await this.$store.dispatch("user/fetchUsers", data.labels);
               data.labels = data.labels.map(label => {
-                return field.formatter(this.getUserByID(label)) || label
-              })
+                return field.formatter(this.getUserByID(label)) || label;
+              });
             } else {
               // Fetch and map records and their values to labels
-              const { namespaceID } = this.chart || {}
-              const recordModule = this.getModuleByID(field.options.moduleID)
+              const { namespaceID } = this.chart || {};
+              const recordModule = this.getModuleByID(field.options.moduleID);
               if (recordModule && data.labels) {
-                await Promise.all(data.labels.map(recordID => {
-                  if (recordID && recordID !== 'undefined') {
-                    return this.$ComposeAPI.recordRead({ namespaceID, moduleID: recordModule.moduleID, recordID }).then(record => {
-                      record = new compose.Record(recordModule, record)
-
-                      if (field.options.recordLabelField) {
-                        // Get actual field
-                        const relatedField = recordModule.fields.find(({ name }) => name === field.options.labelField)
-
-                        return this.$ComposeAPI.recordRead({ namespaceID, moduleID: relatedField.options.moduleID, recordID: record.values[field.options.labelField] }).then(labelRecord => {
-                          record.values[field.options.labelField] = (labelRecord.values.find(({ name }) => name === this.field.options.recordLabelField) || {}).value
-                          return record
+                await Promise.all(
+                  data.labels.map(recordID => {
+                    if (recordID && recordID !== "undefined") {
+                      return this.$ComposeAPI
+                        .recordRead({
+                          namespaceID,
+                          moduleID: recordModule.moduleID,
+                          recordID
                         })
-                      } else {
-                        return record
-                      }
-                    })
-                  } else {
-                    const record = { values: {} }
-                    record.values[field.options.labelField] = recordID
-                    return record
-                  }
-                })).then(records => {
-                  data.labels = records.map(record => {
-                    const value = field.options.labelField ? record.values[field.options.labelField] : record.recordID
-                    return Array.isArray(value) ? value.join(', ') : value
+                        .then(record => {
+                          record = new compose.Record(recordModule, record);
+
+                          if (field.options.recordLabelField) {
+                            // Get actual field
+                            const relatedField = recordModule.fields.find(
+                              ({ name }) => name === field.options.labelField
+                            );
+
+                            return this.$ComposeAPI
+                              .recordRead({
+                                namespaceID,
+                                moduleID: relatedField.options.moduleID,
+                                recordID:
+                                  record.values[field.options.labelField]
+                              })
+                              .then(labelRecord => {
+                                record.values[field.options.labelField] = (
+                                  labelRecord.values.find(
+                                    ({ name }) =>
+                                      name ===
+                                      this.field.options.recordLabelField
+                                  ) || {}
+                                ).value;
+                                return record;
+                              });
+                          } else {
+                            return record;
+                          }
+                        });
+                    } else {
+                      const record = { values: {} };
+                      record.values[field.options.labelField] = recordID;
+                      return record;
+                    }
                   })
-                })
+                ).then(records => {
+                  data.labels = records.map(record => {
+                    const value = field.options.labelField
+                      ? record.values[field.options.labelField]
+                      : record.recordID;
+                    return Array.isArray(value) ? value.join(", ") : value;
+                  });
+                });
               }
             }
           }
 
-          data.labels = data.labels.map(l => l === 'undefined' ? this.$t('chart:undefined') : l)
-          const options = chart.makeOptions(data)
-          const plugins = chart.plugins()
+          data.labels = data.labels.map(l =>
+            l === "undefined" ? this.$t("chart:undefined") : l
+          );
+          const options = chart.makeOptions(data);
+          const plugins = chart.plugins();
           if (!options) {
-            this.toastWarning(this.$t('chart.optionsBuildFailed'))
+            this.toastWarning(this.$t("chart.optionsBuildFailed"));
           }
-          const type = chart.baseChartType(data.datasets)
+          const type = chart.baseChartType(data.datasets);
 
-          const canvas = this.$refs.chartCanvas || undefined
+          const canvas = this.$refs.chartCanvas || undefined;
 
           if (canvas) {
-            const newRenderer = () => new ChartJS(this.$refs.chartCanvas.getContext('2d'), { options, plugins: [...plugins, Funnel, Gauge, csc], data, type })
+            const newRenderer = () =>
+              new ChartJS(this.$refs.chartCanvas.getContext("2d"), {
+                options,
+                plugins: [...plugins, Funnel, Gauge, csc],
+                data,
+                type
+              });
             if (this.renderer) {
-              this.renderer.destroy()
+              this.renderer.destroy();
             }
-            this.renderer = newRenderer()
+            this.renderer = newRenderer();
           }
         } else {
-          data.labels = []
+          data.labels = [];
         }
       } catch (e) {
-        this.toastErrorHandler(this.$t('chart.optionsBuildFailed'))(e)
+        this.toastErrorHandler(this.$t("chart.optionsBuildFailed"))(e);
       }
-      this.$emit('updated')
+      this.$emit("updated");
     },
 
-    requestChartUpdate ({ name, handle } = {}) {
+    requestChartUpdate({ name, handle } = {}) {
       if (!name && !handle) {
-        this.$nextTick(() => this.updateChart())
+        this.$nextTick(() => this.updateChart());
       }
 
       if (name && this.chart && this.chart.name === name) {
-        this.$nextTick(() => this.updateChart())
+        this.$nextTick(() => this.updateChart());
       }
 
       if (handle && this.chart && this.chart.handle === handle) {
-        this.$nextTick(() => this.updateChart())
+        this.$nextTick(() => this.updateChart());
       }
     },
 
-    error (msg) {
+    error(msg) {
       /* eslint-disable no-console */
-      console.error(msg)
-    },
-  },
-}
+      console.error(msg);
+    }
+  }
+};
 </script>
